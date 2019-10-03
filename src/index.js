8ebf0990c9ab0de8ac1cbe7b49d74a91b4f6cb5b
Nikko Osaka <nikko.osaka99@gmail.com>
2019-10-02 20:29:25 -0400

    handle clicks, make Square a function component

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props){
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -29,6 +24,12 @@ class Board extends React.Component {
               onClick={() => this.handleClick(i)}
            />
          );
+  }
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
 
   render() {
