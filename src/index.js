341a1eb2e01785bf2196de6b333384de3690b181
Nikko Osaka <nikko.osaka99@gmail.com>
2019-10-02 20:22:08 -0400

    alter renderSquare function

--- src/index.js
+++ src/index.js
@@ -33,7 +33,10 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return <Square
+              value={this.state.squares[i]}
+              onClick={() => this.handleClick(i)}
+           />;
   }
 
   render() {
